114178af9aee542a75861b66a157738b18fd321a
Youssef Oubelaid <oubelaidyoussef45@gmail.com>
2024-11-26 15:16:43 +0100

    Set api endpoints

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from restaurant import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
